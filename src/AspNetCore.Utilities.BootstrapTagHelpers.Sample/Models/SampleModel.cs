d1c3b99b7baaa5ef8126a07e3ea8ada75e5dcc7c
Mitchel Sellers <msellers@Iowacomputergurus.com>
2022-03-28 15:15:43 -0500

    Initial implementation of #5

---
+++ src/AspNetCore.Utilities.BootstrapTagHelpers.Sample/Models/SampleModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCore.Utilities.BootstrapTagHelpers.Sample.Models
+{
+    public class SampleModel
+    {
+        [Display(Name = "Your Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = "";
+
+        [Display(Name = "Email Address")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+    }
+}
